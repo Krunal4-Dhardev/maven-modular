cee2232681ef4f71a22a7fa915c4ffda426267e6
Krunal Dhardev <63245622+Krunal4-Dhardev@users.noreply.github.com>
2024-03-21 12:00:26 +0530

    Update App.java

--- module2/src/main/java/io/jitpack/App.java
+++ module2/src/main/java/io/jitpack/App.java
@@ -6,10 +6,10 @@ package io.jitpack;
  */
 public class App 
 {
-    public static String GREETING = "Hello World!";
+    public static String GREETING = "Hello World!;
     
     public static void main( String[] args )
     {
-        System.out.println( GREETING );
+        System.out.println( GREETING )
     }
 }
